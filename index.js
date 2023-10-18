3951ee1be77f1f7d03ee04ce1cb7b32610681729
Iam Arjun <iamarjun537@gmail.com>
2023-10-18 20:16:25 +0545

    updated

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ reactApp.use(
 );
 
 reactApp.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", CLIENT_ORIGIN);
+    res.header("Access-Control-Allow-Origin");
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
